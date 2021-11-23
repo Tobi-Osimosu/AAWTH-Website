10fb6de351fd79934ea15a5d98f2c7b4d007428f
Tobi-Osimosu <osimosutobi@gmail.com>
2021-11-23 21:27:57 +0100

    Update

--- src/app/@components/home/home.module.ts
+++ src/app/@components/home/home.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeLandingComponent } from './home-landing/home-landing.component';
+import { Routes, RouterModule } from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+];
 
 @NgModule({
-  declarations: [
-    HomeComponent,
-    HomeLandingComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomeComponent, HomeLandingComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class HomeModule { }
+export class HomeModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
