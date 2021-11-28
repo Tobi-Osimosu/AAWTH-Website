b5ac5850e22f4b135ec765078d63039d0f52c916
Tobi-Osimosu <osimosutobi@gmail.com>
2021-11-29 00:58:06 +0100

    Update

--- src/app/@components/home/home-blog-post/home-blog-post.component.ts
+++ src/app/@components/home/home-blog-post/home-blog-post.component.ts
@@ -9,25 +9,25 @@ export class HomeBlogPostComponent implements OnInit {
   blogPosts = [
     {
       blog_header: 'Celebrating International Day of Women',
-      blog_image: '/assets/image/blog.png',
+      blog_image: '/assets/images/blog.png',
       author: 'ADAOBI O',
       time: '2 days ago',
     },
     {
       blog_header: 'Women leveling up in Tech world',
-      blog_image: '/assets/image/blog.png',
+      blog_image: '/assets/images/blog.png',
       author: 'ADAOBI O',
       time: '2 weeks ago',
     },
     {
       blog_header: 'How has covid’19 accelerated the world of hospitality',
-      blog_image: '/assets/image/blog.png',
+      blog_image: '/assets/images/blog.png',
       author: 'ADAOBI O',
       time: '2 months ago',
     },
     {
       blog_header: 'How can organization improve staff productivity',
-      blog_image: '/assets/image/blog.png',
+      blog_image: '/assets/images/blog.png',
       author: 'ADAOBI O',
       time: '3 months ago',
     },
