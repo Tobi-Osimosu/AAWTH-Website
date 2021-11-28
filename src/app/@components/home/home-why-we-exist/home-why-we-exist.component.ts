0a5d55d81fccadd41e15866d3a1cfeee9d3172ae
Tobi-Osimosu <osimosutobi@gmail.com>
2021-11-28 23:12:57 +0100

    Update

--- src/app/@components/home/home-why-we-exist/home-why-we-exist.component.ts
+++ src/app/@components/home/home-why-we-exist/home-why-we-exist.component.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-home-why-we-exist',
   templateUrl: './home-why-we-exist.component.html',
-  styleUrls: ['./home-why-we-exist.component.scss']
+  styleUrls: ['./home-why-we-exist.component.scss'],
 })
-export class HomeWhyWeExistComponent implements OnInit {
+export class HomeWhyWeExistComponent implements OnInit, AfterViewInit {
+  progressBarInterval: any;
 
-  constructor() { }
+  @ViewChild('progressBar', { static: false }) progressBar!: ElementRef;
 
-  ngOnInit(): void {
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.setProgressBarInterval();
   }
 
+  get progressBarHeight() {
+    if (this.progressBar !== undefined) {
+      return (this.progressBar!.nativeElement as HTMLElement)!.clientHeight;
+    } else {
+      return 0;
+    }
+  }
+
+  setProgressBarInterval() {
+    this.progressBarInterval = setInterval(
+      this.incrementProgressBar.bind(this),
+      500
+    );
+  }
+
+  incrementProgressBar() {
+    let progressBarHeight = (this.progressBar.nativeElement as HTMLElement)
+      .clientHeight;
+
+    if (progressBarHeight < 250) {
+      this.renderer.setStyle(
+        this.progressBar.nativeElement,
+        'height',
+        `${progressBarHeight + 10}px`
+      );
+    } else {
+      this.clearProgressBarInterval();
+    }
+  }
+
+  clearProgressBarInterval() {
+    clearInterval(this.progressBarInterval);
+
+    this.renderer.setStyle(this.progressBar.nativeElement, 'height', `0px`);
+
+    this.setProgressBarInterval();
+  }
 }
