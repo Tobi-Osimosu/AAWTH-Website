bfdb61b7e57bd0369ed9b8ef0cb67322b8e89d9a
Tobi-Osimosu <osimosutobi@gmail.com>
2021-12-29 16:46:50 +0100

    Update

--- src/app/@components/header/header.component.ts
+++ src/app/@components/header/header.component.ts
@@ -1,27 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
   lastScrollTop: number = 0;
 
-  constructor() {}
+  @ViewChild('header', { static: false }) header!: ElementRef;
 
-  ngOnInit(): void {
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
     window.addEventListener(
       'scroll',
       () => {
-        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        let scrollTop =
+          window.pageYOffset || document.documentElement.scrollTop;
+
+        if (scrollTop >= 80) {
+          this.renderer.addClass(this.header.nativeElement, 'fixed-header');
+        } else {
+          if (this.header.nativeElement.classList.contains('fixed-header')) {
+            this.renderer.removeClass(
+              this.header.nativeElement,
+              'fixed-header'
+            );
+          }
+        }
 
         if (scrollTop > this.lastScrollTop) {
           // downscroll code
-          console.log('DownScroll');
+          // console.log('DownScroll');
         } else {
           // upscroll code
-          console.log('UpScroll');
+          // console.log('UpScroll');
         }
 
         this.lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
